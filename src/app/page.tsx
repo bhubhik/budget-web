49a9f79d292f69e9e7093eb779b376cb6daca498
bhubhik <bhubhik@gmail.com>
2023-09-28 11:30:05 +1000

    add month to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import { Dashboard } from '@/components/Dashboard';
 import AddEntry from './entry/page';
 
 export default function Home() {
+  const month = new Date();
   return (
     <main className='flex min-h-screen flex-col items-center justify-center'>
-      <h1>Hello Anubhab!</h1>
+      <h1 className='text-3xl text-yellow-400 mb-8'>Hello Anubhab!</h1>
+      <div className='bg-yellow-300 text-black px-2 py-1 rounded-md mb-4'>
+        <h2>{month.toLocaleString('en-us', { month: 'long' })}</h2>
+      </div>
       <Dashboard />
       <AddEntry />
     </main>
