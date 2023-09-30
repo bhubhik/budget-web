952fab950946c18d23808ba7040741ae0b9f7966
bhubhik <bhubhik@gmail.com>
2023-09-30 10:37:02 +1000

    links to view entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 'use client';
 import { Dashboard } from '@/components/Dashboard';
 import AddEntry from './entry/page';
+import Link from 'next/link';
 
 export default function Home() {
   const month = new Date();
   return (
     <main className='flex min-h-screen flex-col items-center justify-center'>
-      <h1 className='text-3xl text-yellow-400 mb-8'>Hello Anubhab!</h1>
-      <div className='bg-yellow-300 text-black px-2 py-1 rounded-md mb-4'>
+      <h1 className='text-3xl text-yellow-400 mb-4'>Hello Anubhab!</h1>
+      <div>
+        <Link
+          href='/entries?type=expenses'
+          className='mx-4 my-2 underline hover:text-yellow-400'
+        >
+          View Expenses
+        </Link>
+        <Link
+          href='/entries?type=income'
+          className='mx-4 my-2 underline hover:text-yellow-400'
+        >
+          View Income
+        </Link>
+      </div>
+      <div className='bg-yellow-300 text-black px-2 py-1 rounded-md my-8'>
         <h2>{month.toLocaleString('en-us', { month: 'long' })}</h2>
       </div>
       <Dashboard />
