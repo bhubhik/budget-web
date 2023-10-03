92a7382d6659dd514bde9e56258fda03e1b67dac
bhubhik <bhubhik@gmail.com>
2023-10-03 18:47:38 +1100

    delete button

--- src/components/EntryItem.tsx
+++ src/components/EntryItem.tsx
@@ -1,10 +1,15 @@
 import { Entry } from '@/types';
+import { MdDeleteForever } from 'react-icons/md';
 
 interface EntryItemProps {
   entry: Entry;
 }
 
 export default function EntryItem({ entry }: EntryItemProps) {
+  const handleDelete = () => {
+    console.log('delete this item');
+  };
+
   return (
     <div className='bg-white shadow-md rounded-lg overflow-hidden mb-4 w-80'>
       <div className='p-3 flex justify-between'>
@@ -14,7 +19,12 @@ export default function EntryItem({ entry }: EntryItemProps) {
           </h2>
           <p className='text-gray-600 mb-2'>{entry.type}</p>
         </div>
-        <p className='text-2xl font-bold text-green-600'>${entry.amount}</p>
+        <div>
+          <p className='text-2xl font-bold text-green-600'>${entry.amount}</p>
+          <button onClick={handleDelete}>
+            <MdDeleteForever className='text-red-500 text-2xl mt-2' />
+          </button>
+        </div>
       </div>
     </div>
   );
